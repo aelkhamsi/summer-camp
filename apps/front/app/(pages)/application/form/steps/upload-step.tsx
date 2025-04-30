1e8d8f3fca307f543c5409becd25ab2f40c41135
Achraf <achrafelkhamsi@gmail.com>
2025-04-30 18:58:44 +0200

    update legal files in upload step of the application form

--- apps/front/app/(pages)/application/form/steps/upload-step.tsx
+++ apps/front/app/(pages)/application/form/steps/upload-step.tsx
@@ -120,7 +120,7 @@ export const UploadStep = ({
 
             return (
               <FormItem>
-                <FormLabel>Autorisation parentale signée et légalisée par le tuteur légal (<Link className="text-blue-500 underline" href='#' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
+                <FormLabel>Autorisation parentale signée et légalisée par le tuteur légal (<Link className="text-blue-500 underline" href='https://drive.google.com/file/d/1DuvawAEMyHHy-2EKr573bJ2n-rQJSI2h/view?usp=sharing' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
                 <FormControl>
                   <FileInput form={form} id="fileParentalAuthorization" />
                 </FormControl>
@@ -142,7 +142,7 @@ export const UploadStep = ({
 
             return (
               <FormItem>
-                <FormLabel>Règlement signé par l&apos;élève et le tuteur légal(<Link className="text-blue-500 underline" href='#' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
+                <FormLabel>Règlement signé par l&apos;élève et le tuteur légal(<Link className="text-blue-500 underline" href='https://drive.google.com/file/d/14jqiEL7UmfPmz5ekbjpyowHtFUniETtq/view?usp=sharing' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
                 <FormControl>
                   <FileInput form={form} id="fileRegulations" />
                 </FormControl>
